ac56b838785234bd989f647a63b5dab1b1498e95
Beliya470 <anneanziay78@gmail.com>
2023-06-15 22:14:05 +0300

    Completd Assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution in this file!
+let employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// Testing the functions
+console.log("Initial Employee:", employee);
+
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Jane Doe");
+console.log("Updated Employee:", updatedEmployee);
+
+let destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(
+  employee,
+  "streetAddress",
+  "456 Elm St"
+);
+console.log("Destructively Updated Employee:", destructivelyUpdatedEmployee);
+
+let employeeWithoutKey = deleteFromEmployeeByKey(employee, "streetAddress");
+console.log("Employee without Key:", employeeWithoutKey);
+
+let destructivelyDeletedEmployee = destructivelyDeleteFromEmployeeByKey(
+  employee,
+  "name"
+);
+console.log("Destructively Deleted Employee:", destructivelyDeletedEmployee);
+
+console.log("Final Employee:", employee);
